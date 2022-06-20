3605905bd76cf67bd4cb37efd99466b9b25bd5b4
hitska <gobbler.kun@gmail.com>
2022-06-20 22:28:49 +0300

    Улучшил парсинг постов

--- post.py
+++ post.py
@@ -1,10 +1,20 @@
-
 class Post:
     def __init__(self):
         self.id = 0
         self.name = ''
         self.trip = ''
+        self.time = ''
         self.text = ''
+        self.images = []
+        self.answers_to_ids = []
 
     def __str__(self):
-        return f'id {self.id}, name {self.name}, trip {self.trip}: {self.text}'
+        image_count = len(self.images)
+        return f'id({self.id}), name({self.name}), trip({self.trip}), time({self.time}), images({image_count}),'\
+               f' text({self.text})'
+
+class Image:
+    def __init__(self):
+        self.src = ''
+        self.height = 0
+        self.width = 0
